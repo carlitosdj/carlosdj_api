06420aa8ef489323e4f5bf547225476ae2bf25de
Carlos Defelicibus Junior <carlitosceo@gmail.com>
2023-10-06 10:29:28 -0300

    validate user and signup functions inserted

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,14 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+
+const saltOrRounds = 10;
 
 @Injectable()
 export class AuthService {
@@ -9,6 +16,20 @@ export class AuthService {
     private userService: UserService,
     private jwtService: JwtService,
   ) {}
+
+  //new = never used
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.userService.findByEmail(email);
+
+    const isMatchPassword = await bcrypt.compare(pass, user?.password_hash);
+
+    if (user && isMatchPassword) {
+      //const { password_hash, ...result } = user;
+      const { ...result } = user;
+      return result;
+    }
+    return null;
+  }
 
   async signIn(email: string, pass: string): Promise<any> {
     const user = await this.userService.findByEmail(email);
@@ -29,5 +50,23 @@ export class AuthService {
     // //instead of the user object
 
     // return result;
+  }
+
+  //new = never used
+  async signUp(email: string, pass: string): Promise<any> {
+    const user = await this.userService.findByEmail(email);
+    if (user) {
+      throw new HttpException('Username taken', HttpStatus.BAD_REQUEST);
+    }
+
+    const hash = await bcrypt.hash(pass, saltOrRounds);
+    const date = new Date();
+    const newUser = await this.userService.create({
+      email,
+      password_hash: hash,
+      created_at: date.getTime() / 1000,
+      name: 'teste',
+    });
+    return newUser;
   }
 }
