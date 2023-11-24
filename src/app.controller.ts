75ad7358bf09bb050210c95d77845afba040991e
Carlos Defelicibus Junior <carlitosceo@gmail.com>
2023-11-24 16:51:52 -0300

    Fix CICD

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,40 +12,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @SkipAuth()
-  @Get('teste')
-  getTest2(): string {
-    return this.appService.getTest2();
-  }
-
-  @SkipAuth()
-  @Get('teste3')
-  getTest3(): string {
-    return this.appService.getTest3();
-  }
-
-  @SkipAuth()
-  @Get('teste4')
-  getTest4(): string {
-    return this.appService.getTest4();
-  }
-
-  @SkipAuth()
-  @Get('teste5')
-  getTest5(): string {
-    return this.appService.getTest5();
-  }
-
-  @SkipAuth()
-  @Get('teste6')
-  getTest6(): string {
-    return this.appService.getTest6();
-  }
-
-  @SkipAuth()
-  @Get('teste7')
-  getTest7(): string {
-    return this.appService.getTest7();
-  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,27 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
-  }
-
-  getTest2(): string {
-    return 'Teste2!';
-  }
-
-  getTest3(): string {
-    return 'Teste3!';
-  }
-  getTest4(): string {
-    return 'Teste4!';
-  }
-  getTest5(): string {
-    return 'Teste5!';
-  }
-  getTest6(): string {
-    return 'Teste6!';
-  }
-
-  getTest7(): string {
-    return 'Teste7!';
+    return 'Servidor Instituto Defelícibus - Api MODELO';
   }
 }
