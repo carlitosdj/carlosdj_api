f65226ed4c185e16488932db2ee2d28e49972c10
Carlos Defelicibus Junior <carlitosceo@gmail.com>
2023-12-01 17:31:10 -0300

    correction upload

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -7,7 +7,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '../../upload/');
+    cb(null, '../../../upload/');
   },
   filename: (req, file, cb) => {
     //cb(null, `${Date.now()}_${file.originalname}`);
