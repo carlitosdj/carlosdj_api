d360c299a2bfc16e1415bed6258549450689d361
Carlos Defelicibus Junior <carlitosceo@gmail.com>
2023-12-01 15:19:55 -0300

    cors problem

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     },
   );
 
-  //app.enableCors();
+  app.enableCors();
   app.useGlobalFilters(
     //new PrismaExceptionFilter(),
     new InvalidRelationExceptionFilter(),
