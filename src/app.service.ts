1a27325909be80e4f977aa971ffd6c393fbd9956
Carlos Defelicibus Junior <carlitosceo@gmail.com>
2024-01-09 18:02:29 -0300

    Welcome message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Servidor Instituto Defelícibus - Api modelo';
+    return 'Servidor Instituto Defelícibus - Api CARLOSDJ';
   }
 }
